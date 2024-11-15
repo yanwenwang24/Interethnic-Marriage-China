c1bc917b620bd65444f00d1126f8087f333f67ec
Yanwen Wang <yanwenwang@u.nus.edu>
2024-11-15 14:18:49 +0800

    Rename generated graphs

--- 15.1_loglinear_ethngrp_Huabei.jl
+++ 15.1_loglinear_ethngrp_Huabei.jl
@@ -475,7 +475,7 @@ legend!(f[1, 2], plt)
 
 f
 
-save("graphs/inter_odds_Huabei.png", f; px_per_unit=2)
+save("graphs/inter_odds_ethngrp_Huabei.png", f; px_per_unit=2)
 
 # Expotentiate coefficients
 @chain odd_ratio_df begin
