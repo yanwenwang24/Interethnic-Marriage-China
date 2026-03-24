4a22bb15f81d7d33ff9c7c8b6fde5cd27d3467b3
Yanwen Wang <wenbolyanwen@me.com>
2026-03-24 20:36:51 +0800

    Add printing of tables

--- scripts_visualize/fig1_trends_by_ethngrp.R
+++ scripts_visualize/fig1_trends_by_ethngrp.R
@@ -10,10 +10,14 @@
 df <- read_feather("data/visualization/fig1_trends_by_ethngrp.arrow") |>
   mutate(ethngrp = factor(ethngrp, levels = ethngrp_order))
 
+arrange(df, ethngrp, year) %>%
+  mutate(prop = sprintf("%.2f", prop * 100)) %>%
+  print(n = Inf)
+
 p <- ggplot(df, aes(x = year, y = prop)) +
   geom_line(linewidth = 0.7, color = "grey30") +
   geom_point(size = 2, color = "grey30") +
-  facet_wrap(~ ethngrp, nrow = 3) +
+  facet_wrap(~ethngrp, nrow = 3) +
   scale_x_continuous(breaks = as.numeric(year_order)) +
   scale_y_continuous(
     labels = percent_format(),
@@ -24,6 +28,7 @@ p <- ggplot(df, aes(x = year, y = prop)) +
   theme_intermarriage()
 
 ggsave("figures/trends_by_ethngrp.pdf", p,
-       width = 6.5, height = 6, device = cairo_pdf)
+  width = 6.5, height = 6, device = cairo_pdf
+)
 
 message("  Figure 1: trends_by_ethngrp.pdf")
